97be4b241fa5e1e03974c29191dbad05872b1f24
johnwunderfauks <john@wunderfauks.com>
2026-03-10 10:59:02 +0800

    Update redisClient.js

--- helpers/services/redisClient.js
+++ helpers/services/redisClient.js
@@ -3,6 +3,8 @@ const Redis = require("ioredis");
 let redis;
 
 function getRedis() {
+  console.log("[debug] REDIS_URL present:", Boolean(process.env.REDIS_URL));
+console.log("[debug] REDIS_URL length:", process.env.REDIS_URL ? process.env.REDIS_URL.length : 0);
   if (redis) return redis;
 
   if (!process.env.REDIS_URL) {
@@ -11,7 +13,7 @@ function getRedis() {
 
   redis = new Redis(process.env.REDIS_URL, {
     maxRetriesPerRequest: null,
-    enableReadyCheck: true,
+    enableReadyCheck: false,
   });
 
   redis.on("connect", () => {
